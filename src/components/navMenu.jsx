5e0699e343c8bdf3a2a3fc4baa627c271cb86f74
Med-azaza <hamaaz666@gmail.com>
2024-05-27 11:15:36 +0100

    fix dynamic nav menu

--- src/components/navMenu.jsx
+++ src/components/navMenu.jsx
@@ -1,17 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const NavMenu = () => {
+  const location = useLocation();
+  const pathname = location.pathname;
+  const currentPage = pathname.substring(1);
+
   return (
     <ul className="nav-list">
       <li>
         <Link to="/">Home</Link>
       </li>
-      <li>
-        <Link to="/work">Work</Link>
-      </li>
-      <li>
-        <Link to="/contact">Contact</Link>
-      </li>
+      {currentPage !== "about" && (
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      )}
+      {currentPage !== "work" && (
+        <li>
+          <Link to="/work">Work</Link>
+        </li>
+      )}
+      {currentPage !== "contact" && (
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      )}
     </ul>
   );
 };
