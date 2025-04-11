b12bd62a6032ad049d0f8dd2b33360e6bb85529c
Med-azaza <hamaaz666@gmail.com>
2025-04-11 12:28:18 +0100

    add nav menu on work page

--- src/components/workPage.jsx
+++ src/components/workPage.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Projects from "./projects";
 import Timeline from "./timeline";
+import NavMenu from "./navMenu";
 
 const WorkPage = () => {
   const [activeSection, setActiveSection] = useState("");
   return (
     <div className="work-page-container">
+      <NavMenu />
       <Projects
         setActiveSection={setActiveSection}
         activeSection={activeSection}
