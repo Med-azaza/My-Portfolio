8b6901d2bce8a45658286e0731ef8c9fe5f1a733
Med-azaza <hamaaz666@gmail.com>
2025-04-11 13:34:41 +0100

    add nav menu icons

--- src/components/navMenu.jsx
+++ src/components/navMenu.jsx
@@ -1,4 +1,10 @@
 import { Link, useLocation } from "react-router-dom";
+import {
+  FaHouse,
+  FaBriefcase,
+  FaAddressCard,
+  FaPaperPlane,
+} from "react-icons/fa6";
 
 const NavMenu = () => {
   const location = useLocation();
@@ -8,20 +14,24 @@ const NavMenu = () => {
   return (
     <ul className="nav-list">
       <li>
+        <FaHouse size={30} className="icon" />
         <Link to="/">Home</Link>
       </li>
       {currentPage !== "about" && (
         <li>
+          <FaAddressCard size={30} className="icon" />
           <Link to="/about">About</Link>
         </li>
       )}
       {currentPage !== "work" && (
         <li>
+          <FaBriefcase size={30} className="icon" />
           <Link to="/work">Work</Link>
         </li>
       )}
       {currentPage !== "contact" && (
         <li>
+          <FaPaperPlane size={30} className="icon" />
           <Link to="/contact">Contact</Link>
         </li>
       )}
